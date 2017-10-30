2085cef8cbeb2649852edf18a7b476a2e3d01e27
darkfire5900 <caldwellliam@gmail.com>
2017-10-30 07:38:07 -0400

    Update run.py

--- run.py
+++ run.py
@@ -12,18 +12,17 @@ USERNAME = config.get('required', 'username')
 PLAYLIST_ID = config.get('required', 'playlist_uri')
 
 
-track_ids, artist_ids = s.return_track_artist_ids(USERNAME, PLAYLIST_ID)
+track_ids, artist_ids = s.playlist_tracks(USERNAME, PLAYLIST_ID)
 
-related_artists = set(s.return_related_artists(artist_ids))
-related_albums = s.return_artists_albums(related_artists)
-album_tracks = s.return_artist_album_tracks(related_albums)
+related_artists = set(s.related_artists(artist_ids))
+related_albums = s.artist_albums(related_artists)
+album_tracks = s.album_tracks(related_albums)
 
-other_track_analysis = s.return_track_analysis(album_tracks)
-my_track_analysis = s.return_track_analysis(track_ids)
+other_track_analysis = s.track_analysis(album_tracks)
+my_track_analysis = s.track_analysis(track_ids)
 
-df = pd.DataFrame(other_track_analysis)
-df2 = pd.DataFrame(my_track_analysis)
+other_tracks = pd.DataFrame(other_track_analysis)
+my_tracks = pd.DataFrame(my_track_analysis)
 
-
-feather.write_dataframe(df, 'dat/other_tracks.feather')
-feather.write_dataframe(df2, 'dat/my_tracks.feather')
+feather.write_dataframe(other_tracks, 'dat/other_tracks.feather')
+feather.write_dataframe(my_tracks, 'dat/my_tracks.feather')
